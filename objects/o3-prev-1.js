5d184bcc4094b2c86e01b9e62da41bc558e62259
Santi Lubary <santiago.lp.cop@gmail.com>
2025-06-23 20:56:24 -0300

    asdf

---
+++ objects/o3-prev-1.js
@@ -0,0 +1,42 @@
+/**
+ * 
+*/
+
+const experienceGainedLog = [
+    {name:'maria', exp: 4}, 
+    {name:'manuel', exp: 9}, 
+    {name:'carlos', exp: 7},  
+    {name:'jose', exp: 8}, 
+    {name:'ana', exp: 5}, 
+    {name:'juan', exp: 2}, 
+    {name:'luis', exp: 6}, 
+    {name:'santi', exp: 1}, 
+    {name:'pedro', exp: 3},
+    {name:'jose', exp: 8}, 
+    {name:'ana', exp: 5}, 
+    {name:'juan', exp: 2}, 
+    {name:'luis', exp: 6}, 
+    {name:'santi', exp: 1}, 
+    {name:'pedro', exp: 3},
+    {name:'jose', exp: 8}, 
+    {name:'ana', exp: 5}, 
+    {name:'juan', exp: 2}, 
+    {name:'luis', exp: 6}, 
+    {name:'santi', exp: 1}, 
+    {name:'pedro', exp: 3},
+];
+
+function checkUserExists(userName, experienceGainedLog) {
+    // first check if the user exists in the experienceGainedLog
+}
+
+function getTotalExpForUser(userName, experienceGainedLog) {
+    const userExists = checkUserExists(userName, experienceGainedLog)
+    if (userExists) {
+        // do somethign to get the total amount of exp for the user if found
+    }
+    return "Error: User not found"
+}
+
+getTotalExpForUser('jose', experienceGainedLog)
+
